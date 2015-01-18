cbdc47d28e5570d0ca4017e1aa658fcc640fd4e6
Alan <alanng95@hotmail.com>
2015-01-19 02:13:46 +1100

    fixed arduino

--- Code/LEDPWMRGB/LEDPWMRGB.ino
+++ Code/LEDPWMRGB/LEDPWMRGB.ino
@@ -8,7 +8,8 @@ void setup() {
   Serial.begin(9600); // Tuen serial monitor on
   pinMode(ledR, OUTPUT); // Make pin 3 an output
   pinMode(ledG, OUTPUT); // Make pin 5 an output
-  pinMode(ledB, OUTPUT); // Make pin 6 an output 
+  pinMode(ledB, INTPUT); // Make pin 6 an output 
+  
 }
 
 // Fade in a LED
